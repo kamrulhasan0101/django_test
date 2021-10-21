64c86d763641094c585bfcfeec44a69601445c12
kamrulhasan0101 <kamrulhasan0101@gmail.com>
2021-10-22 03:30:08 +0600

    form submission and dispay data

--- first_module/views.py
+++ first_module/views.py
@@ -6,3 +6,10 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name': 'Kamrul Hasan'})
+
+
+def add(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    result = val1 + val2
+    return render(request, 'result.html', {'result': result})
