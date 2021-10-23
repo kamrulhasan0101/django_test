23b1105237f6911a174c1b1efa7ee3ec8fa61b13
Kamrul Hasan <kamrulhasan0101@gmail.com>
2021-10-23 18:58:33 +0600

    passing array to html and fetch with for loop

--- event/models.py
+++ event/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event:
+    id: int
+    image: str
+    deadline: str
+    title: str
+    description: str
+
+    def __init__(self, title, deadline):
+        self.title = title
+        self.deadline = deadline
--- event/views.py
+++ event/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Event
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    event1 = Event('pic1', '22')
+    event2 = Event('pic2', '33')
+    event3 = Event('pic3', '44')
+    event4 = Event('pic4', '55')
+    events = [event1, event2, event3, event4]
+    return render(request, 'index.html', {'events': events})
