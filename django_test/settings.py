bff5e9d3c94e9488501bda93e3efec6466850474
Kamrul Hasan <kamrulhasan0101@gmail.com>
2021-10-24 18:17:39 +0600

    database connection and make table with migration

--- django_test/settings.py
+++ django_test/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'event.apps.EventConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'django_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_test',
+        'USER': 'django_test',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
--- event/models.py
+++ event/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
 
-class Event:
-    id: int
-    image: str
-    deadline: str
-    title: str
-    description: str
+class Event(models.Model):
+    title = models.TextField()
+    description = models.TextField(default=None)
+    date = models.DateField(null=True, blank=True, default=None)
+    img = models.ImageField(upload_to='pics')
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    updated_at = models.DateTimeField(default=datetime.now, blank=True)
 
-    def __init__(self, title, deadline):
-        self.title = title
-        self.deadline = deadline
