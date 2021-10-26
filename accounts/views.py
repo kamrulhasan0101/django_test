ce39c15f3b422d78e0a160e96677eb42d64b57a0
Kamrul Hasan <kamrulhasan0101@gmail.com>
2021-10-26 18:12:15 +0600

    log out done

--- accounts/views.py
+++ accounts/views.py
@@ -45,3 +45,9 @@ def login(request):
             return redirect('accounts:login')
     else:
         return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
